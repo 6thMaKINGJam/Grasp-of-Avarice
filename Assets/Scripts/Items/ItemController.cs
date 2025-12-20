f4c70004e8729302b2384bd73120877b2b0e5041
tykim0O0 <emily9943@naver.com>
2025-12-20 12:11:12 +0900

    Item 코드 조금 수정

---
+++ Assets/Scripts/Items/ItemController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    // 플레이어와 닿아 있는 아이템 목록 수집
+
+    // 수집한 아이템 중 우선순위가 가장 높은 아이템 획득 (OnPicked 호출)
+
+    // 획득한 아이템 목록에서 제거
+
+    // 
+}
