8994102862670d54454aed02a1394ab1188772e4
CinnaPie <73657005+CinnaPie@users.noreply.github.com>
2025-12-20 11:24:38 +0900

    feat : 금화 추가
    Goldcoin 오브젝트 추가

--- Assets/Scripts/Items/Coin.cs
+++ Assets/Scripts/Items/Coin.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    private bool isPlayerNearby = false;
     // Update is called once per frame
     void Update()
     {
-        
+        if(isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            //아이템 먹기
+            Debug.Log("먹음");
+            Destroy(gameObject); //아직 인벤 저장 구현이 안 됨
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player"){
+            //현재 플레이어와 닿은 상태
+            isPlayerNearby = true;
+            Debug.Log("닿음!");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            isPlayerNearby = false;
+            Debug.Log("멀어짐");
+        }
     }
 }
