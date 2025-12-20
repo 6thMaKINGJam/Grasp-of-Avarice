d60890ebfe055c485883ddc511354b39e74eebc7
tykim0O0 <emily9943@naver.com>
2025-12-20 19:47:47 +0900

    폭탄 구현

--- Assets/Scripts/Items/Bomb.cs
+++ Assets/Scripts/Items/Bomb.cs
@@ -1,11 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Bomb : Item
 {
+    private void Awake()
+    {
+        ExplodeAfterDelay();
+    }
+    // 지정한 시간(초) 후에 Explode를 호출하는 코루틴
+    private IEnumerator ExplodeDelayedCoroutine(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Explode();
+    }
+
+    // 외부에서 1초 지연으로 폭발을 시작하기 위한 인터페이스
+    public void ExplodeAfterDelay(float delay = 1f)
+    {
+        StartCoroutine(ExplodeDelayedCoroutine(delay));
+    }
+
     public void Explode()
     {
-        Debug.Log("폭발해서 벽을 파괴하기");
+        float breakRadius = 1.1f;
+        Vector3 bombPos = transform.position;
+
+        // 등록된 모든 BreakableTilemap을 검사
+        foreach (var breakable in BreakableTilemap.Instances)
+        {
+            if (breakable == null) continue;
+
+            var tm = breakable.GetComponent<Tilemap>();
+            if (tm == null) continue;
+
+            // 폭탄의 위치를 해당 타일맵의 셀 좌표로 변환
+            Vector3Int centerCell = tm.WorldToCell(bombPos);
+
+            // 좌우 한 칸을 검사
+            var checkCells = new[] { centerCell + Vector3Int.left, centerCell + Vector3Int.right };
+
+            foreach (var cell in checkCells)
+            {
+                if (!tm.HasTile(cell)) continue;
+
+                // 해당 셀의 월드 중심 좌표를 BreakTiles에 전달
+                Vector3 cellWorld = tm.GetCellCenterWorld(cell);
+                breakable.BreakTiles(cellWorld, breakRadius);
+            }
+        }
     }
 }
