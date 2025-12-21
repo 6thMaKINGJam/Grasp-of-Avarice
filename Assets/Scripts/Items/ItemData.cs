001583c27bb0aeaeea428a850f97c353770a4e77
CinnaPie <73657005+CinnaPie@users.noreply.github.com>
2025-12-21 14:01:14 +0900

    Feat : 훼이크용 보석 추가 🥰

--- Assets/Scripts/Items/ItemData.cs
+++ Assets/Scripts/Items/ItemData.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public enum ItemType {Coin = 0, Bomb = 1, Box = 2, Bottle = 3, Key = 4, Feather = 5, Hat = 6}
+public enum ItemType {Coin = 0, Bomb = 1, Box = 2, Bottle = 3, Key = 4, Feather = 5, Hat = 6, Gem=7}
 [CreateAssetMenu(menuName = "Game/Item Data", fileName = "ItemData")]
 public class ItemData : ScriptableObject
 {
