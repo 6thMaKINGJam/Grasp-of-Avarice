2d25215d109a895e5d8cee02a15e12248bc9bcb8
samchodev <samchodev@gmail.com>
2025-12-23 12:07:09 +0900

    add arrow key binding for animation

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -14,10 +14,12 @@ public class PlayerAnimation : MonoBehaviour
 
     private void Update()
     {
-        bool isMoving = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        bool isMoving = horizontal != 0; 
         animator.SetBool("IsRightWalking", isMoving);
-
-        bool isClimbingKey = (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)) && animator.GetBool("IsClimbing");
+        
+        float vertical = Input.GetAxisRaw("Vertical");
+        bool isClimbingKey = (vertical != 0) && animator.GetBool("IsClimbing");
         animator.SetBool("IsClimbingKey", isClimbingKey);
     }
 }
